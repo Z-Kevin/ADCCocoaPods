cccca4298f9646c8999eb3bb72593fc2fe79d8b1
张晓宇 <zhangxiaoyu@91jinrong.com>
2018-05-07 10:14:38 +0800

    commit code
    creat first project

---
+++ ADCCocoaPods/ADCCocoaPods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ADCCocoaPods
+//
+//  Created by ZXY on 2018/5/7.
+//  Copyright © 2018年 JinRongBaGuaNv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ADCCocoaPods/ADCCocoaPods/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ADCCocoaPods
+//
+//  Created by ZXY on 2018/5/7.
+//  Copyright © 2018年 JinRongBaGuaNv. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
